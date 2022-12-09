99bbe883a13d45e9461d6a2e9e38f218b17c6230
Jonathan van der Steege <jonathan@jonakeys.nl>
2022-12-09 22:14:31 +0100

    Day 8 part 2 and optimization

--- day8/day8.java
+++ day8/day8.java
@@ -9,6 +9,8 @@ class day8
 		new ArrayList<ArrayList<Integer>>();
 	private static ArrayList<ArrayList<Integer>> visibilityMap =
 		new ArrayList<ArrayList<Integer>>();
+	private static ArrayList<ArrayList<Integer>> scenicScoreMap =
+		new ArrayList<ArrayList<Integer>>();
 	private static int width = 0;
 	private static int height = 0;
 
@@ -20,39 +22,51 @@ class day8
 			String line = scanner.nextLine();
 			ArrayList<Integer> mapRow = new ArrayList<Integer>();
 			ArrayList<Integer> visiMapRow = new ArrayList<Integer>();
+			ArrayList<Integer> scenicRow = new ArrayList<Integer>();
+			
 			for (int i = 0; i < line.length(); ++i) {
 				mapRow.add(Integer.parseInt(String.valueOf(line.charAt(i))));
 				visiMapRow.add(1); // default visible
+				scenicRow.add(0);
 			}
 			map.add(mapRow);
 			visibilityMap.add(visiMapRow);
+			scenicScoreMap.add(scenicRow);
 		}
 		scanner.close();
 
 		int visibleTrees = 0;
+		int highestScenicScore = 0;
 		width = map.get(0).size();
 		height = map.size();
 		visibleTrees += (width*2 + height*2) - 4;
-		System.out.println("Outside visible: " + visibleTrees);
+		System.out.println("From outside visible: " + visibleTrees);
 
 		// Part 1
 		for (int y = 1; y < height-1; ++y) {
 			for (int x = 1; x < width-1; ++x) {
 				visibilityMap.get(y).set(x, setVisibility(x, y));
-			}
-		}
-		for (int y = 1; y < height-1; ++y) {
-			for (int x = 1; x < width-1; ++x) {
 				visibleTrees += visibilityMap.get(y).get(x);
 			}
 		}
 		System.out.println("Total trees visible: " + visibleTrees);
 
 		// Part 2
+		for (int y = 0; y < height; ++y) {
+			for (int x = 0; x < width; ++x) {
+				int treeScenicScore = scenicScore(x, y);
+				scenicScoreMap.get(y).set(x, treeScenicScore);
+				if (treeScenicScore > highestScenicScore)
+					highestScenicScore = treeScenicScore;
+			}
+		}
+
+		System.out.println("Highest scenic score: " + highestScenicScore);
+		
 	}
 
 	static int setVisibility(int x, int y) {
-		int treeNumber = map.get(y).get(x);
+		int treeHeight = map.get(y).get(x);
 
 		// true = visible, false = invisible
 		boolean left = true;
@@ -62,28 +76,28 @@ class day8
 		
 		// check visible from left
 		for (int i = 0; i < x; ++i) {
-			if (!isVisible(i, y, treeNumber)) {
+			if (!isVisible(i, y, treeHeight)) {
 				left = false;
 				break;
 			}
 		}
 		// check visible from right
 		for (int i = width - 1; i > x; --i) {
-			if (!isVisible(i, y, treeNumber)) {
+			if (!isVisible(i, y, treeHeight)) {
 				right = false;
 				break;
 			}
 		}
 		// check visible from top
 		for (int i = 0; i < y; ++i) {
-			if (!isVisible(x, i, treeNumber)) {
+			if (!isVisible(x, i, treeHeight)) {
 				top = false;
 				break;
 			}
 		}
 		// check visible from down
 		for (int i = height - 1; i > y; --i) {
-			if (!isVisible(x, i, treeNumber)) {
+			if (!isVisible(x, i, treeHeight)) {
 				down = false;
 				break;
 			}
@@ -94,7 +108,53 @@ class day8
 		else return 1;
 	}
 
-	static boolean isVisible(int x, int y, int treeNumber) {
-		return map.get(y).get(x) < treeNumber;
+	static boolean isVisible(int x, int y, int treeHeight) {
+		return map.get(y).get(x) < treeHeight;
+	}
+
+	static int scenicScore(int x, int y) {
+		int left = 0;
+		int right = 0;
+		int top = 0;
+		int down = 0;
+		int treeHeight = map.get(y).get(x);
+
+		// scenic left
+		if (x > 0) {
+			for (int i = x-1; i >= 0; --i) {
+				left++;
+				if (map.get(y).get(i) >= treeHeight)
+					break;
+			}
+		}
+			
+		// scenic right
+		if (x < width - 1) {
+			for (int i = x+1; i < width; ++i) {
+				right++;
+				if (map.get(y).get(i) >= treeHeight)
+					break;
+			}
+		}
+		
+		// scenic top
+		if (y > 0) {
+			for (int i = y-1; i >= 0; --i) {
+				top++;
+				if (map.get(i).get(x) >= treeHeight)
+					break;
+			}
+		}
+		
+		// scenic down
+		if (y < height - 1) {
+			for (int i = y+1; i < height; ++i) {
+				down++;
+				if (map.get(i).get(x) >= treeHeight)
+					break;
+			}
+		}
+
+		return left * right * top * down;
 	}
 }
