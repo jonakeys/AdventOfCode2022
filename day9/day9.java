6d0cae138dc381f7f5d4bb374474d6253a8adc85
Jonathan van der Steege <jonathan@jonakeys.nl>
2022-12-10 00:06:45 +0100

    Day 9 start

---
+++ day9/day9.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class day9
+{
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("test.txt");
+		Scanner scanner = new Scanner(file);
+
+		
+		
+		scanner.close();
+	}
+}
